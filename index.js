4600b99509e47e3d8830ba1cb78a4672fdcb898a
kru1980 <kru71101@gmail.com>
2019-10-12 13:00:42 +0500

    start

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", function(req, res) {
+  res.send("Hello World Rodik!");
+});
+
+app.listen(3000, function() {
+  console.log("Example app listening on port 3000!");
+});
