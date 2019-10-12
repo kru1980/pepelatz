668a8ce29b6faeb6afa0cd8ac883d9d690bfb5ad
kru1980 <kru71101@gmail.com>
2019-10-12 13:52:26 +0500

    template

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const config = require("./config/config");
+const db = require("./config/db");
 
 const app = express();
 
+// mongo start
+mongoose
+  .connect(db.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("local db connected..");
+  })
+  .catch(err => console.log(err));
+// mongo end
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", function(req, res) {
-  res.send("Hello World Rodik!");
+  res.render("index");
 });
 
-app.listen(3000, function() {
-  console.log("Example app listening on port 3000!");
+app.listen(config.PORT, function() {
+  console.log("Example app listening on port !" + config.PORT);
 });
