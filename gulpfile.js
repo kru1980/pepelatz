3ba1e8b2c4c655c06cc212ca4c41171f3f061232
kru1980 <kru71101@gmail.com>
2019-10-13 09:46:43 +0500

    add gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,48 @@
+/* eslint-disable node/no-unpublished-require */
 const gulp = require("gulp");
 const sass = require("gulp-sass");
 const autoprefixer = require("gulp-autoprefixer");
 const cssnano = require("gulp-cssnano");
+const plumber = require("gulp-plumber");
+const concat = require("gulp-concat");
+const uglify = require("gulp-uglifyjs");
+/* eslint-enable node/no-unpublished-require */
 
-gulp.task("scss", function(done) {
+function scss() {
+  return gulp
+    .src("dev/scss/**/*.scss")
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(
+      autoprefixer(["last 15 versions", "> 1%", "ie 8", "ie 7"], {
+        cascade: true
+      })
+    )
+    .pipe(cssnano())
+    .pipe(gulp.dest("public/stylesheets"));
+}
+
+function scripts() {
   return (
     gulp
-      .src("dev/scss/**/*.scss")
-
-      .pipe(sass())
-      .pipe(
-        autoprefixer(["last 15 versions", "> 1%", "ie 8", "ie 7"], {
-          cascade: true
-        })
-      )
-      // .pipe(cssnano())
-      .pipe(gulp.dest("dist/css"))
+      .src([
+        "dev/js/auth.js",
+        "dev/js/post.js",
+        "dev/js/comment.js"
+        //
+      ])
+      .pipe(concat("scripts.js"))
+      // .pipe(uglify())
+      .pipe(gulp.dest("public/javascripts"))
   );
+}
+function watch() {
+  gulp.watch("dev/scss/**/*.scss", gulp.series("scss"));
+  gulp.watch("dev/js/**/*.js", gulp.series("scripts"));
+}
 
-  done();
-});
+gulp.task(scss);
+gulp.task(scripts);
+gulp.task(watch);
 
-gulp.watch("dev/scss/**/*.scss", gulp.series("scss"));
-
-gulp.task("default", gulp.series("scss"));
+module.exports.default = gulp.series(scss, scripts, watch);
