35be550fca7b3b250bba0a1dc9c66f0dda388605
kru1980 <kru71101@gmail.com>
2019-10-24 22:46:41 +0500

    add post rout

--- routes/post.js
+++ routes/post.js
@@ -1,25 +1,68 @@
 const express = require("express");
 const router = express.Router();
+const TurndownService = require("turndown");
 
 const models = require("../models");
 
-const User = models.User;
-
+// GET for add
 router.get("/add", (req, res) => {
   const id = req.session.userId;
   const login = req.session.userLogin;
 
   res.render("post/add", {
-    user: { id, login }
+    user: {
+      id,
+      login
+    }
   });
 });
 
+// POST is add
 router.post("/add", (req, res) => {
-  console.log(req.body);
+  const title = req.body.title.trim().replace(/ +(?= )/g, "");
+  const body = req.body.body;
+  const turndownService = new TurndownService();
 
-  res.json({
-    ok: true
-  });
+  if (!title || !body) {
+    const fields = [];
+    if (!title) fields.push("title");
+    if (!body) fields.push("body");
+
+    res.json({
+      ok: false,
+      error: "Все поля должны быть заполнены!",
+      fields
+    });
+  } else if (title.length < 3 || title.length > 64) {
+    res.json({
+      ok: false,
+      error: "Длина заголовка от 3 до 64 символов!",
+      fields: ["title"]
+    });
+  } else if (body.length < 3) {
+    res.json({
+      ok: false,
+      error: "Текст не менее 3х символов!",
+      fields: ["body"]
+    });
+  } else {
+    models.Post.create({
+      title,
+      body: turndownService.turndown(body)
+    })
+      .then(post => {
+        console.log(post);
+        res.json({
+          ok: true
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.json({
+          ok: false
+        });
+      });
+  }
 });
 
 module.exports = router;
